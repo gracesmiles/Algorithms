15b62ab2d4430effb7cd8bbfa9593aa42acbeeb3
Grace Best <136754141+gracesmiles@users.noreply.github.com>
2025-06-26 13:46:39 -0400

    Update LinearSearch.py

--- LinearSearch.py
+++ LinearSearch.py
@@ -13,3 +13,9 @@ def linear_search(arr, target):
 arr = [3, 5, 2, 4, 9]
 target = 4
 linear_search(arr, target)
+
+"""
+Best-case scenario O(1): The target is the first list element.
+Worst-case scenario O(n): The target is the last element or not present.
+Average case O(n): Assuming uniform distribution, the search examines half the list, simplifying to O(n)
+"""
